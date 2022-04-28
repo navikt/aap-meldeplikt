e4b12765d9342b063edac69bae45c54e865f9afc
Richard Borge <richard.borge@nav.no>
2022-04-28 10:55:08 +0200

    Setter opp hendelse

---
+++ app/src/main/kotlin/no/nav/aap/meldeplikt/Meldepliktshendelse.kt
@@ -0,0 +1,24 @@
+package no.nav.aap.meldeplikt
+
+import java.time.LocalDate
+import java.util.Random
+
+internal data class Meldepliktshendelse(
+    val brukersAktivitet: List<BrukeraktivitetPerDag>
+)
+
+internal data class BrukeraktivitetPerDag(
+    val dato: LocalDate,
+    val arbeidstimer: Double,
+    val fraværsdag: Boolean
+)
+
+internal fun createRandomHendelse() = Meldepliktshendelse (
+    brukersAktivitet = (13L downTo 0).map {
+        BrukeraktivitetPerDag(
+            dato = LocalDate.now().minusDays(it),
+            arbeidstimer = listOf(0.0, 3.0, 7.5).random(),
+            fraværsdag = Random().nextInt() > 75
+        )
+    }
+)
